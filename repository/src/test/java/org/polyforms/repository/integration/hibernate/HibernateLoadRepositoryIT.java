8c633237a9da3321151c648b6b31d9b10682b7b9
Kuisong Tong <kuisong.tong@polyforms.org>
2011-08-31 16:56:55 +0800

    remove LoadBy executor.

--- repository/src/test/java/org/polyforms/repository/integration/hibernate/HibernateLoadRepositoryIT.java
+++
@@ -1,8 +0,0 @@
-package org.polyforms.repository.integration.hibernate;
-
-import org.polyforms.repository.integration.LoadRepositoryIT;
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration("hibernate-context.xml")
-public class HibernateLoadRepositoryIT extends LoadRepositoryIT {
-}
--- repository/src/test/java/org/polyforms/repository/integration/openjpa/OpenJpaLoadRepositoryIT.java
+++
@@ -1,8 +0,0 @@
-package org.polyforms.repository.integration.openjpa;
-
-import org.polyforms.repository.integration.LoadRepositoryIT;
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration("openJpa-context.xml")
-public class OpenJpaLoadRepositoryIT extends LoadRepositoryIT {
-}
