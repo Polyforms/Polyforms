a58feb3de9bde4c515b60b8a7b66d6ebaf34c532
Kuisong Tong <kuisong.tong@polyforms.org>
2011-12-28 14:26:05 +0800

    remove duplication of code.

--- repository/src/main/java/org/polyforms/repository/jpa/query/CountQueryStringBuilder.java
+++ repository/src/main/java/org/polyforms/repository/jpa/query/CountQueryStringBuilder.java
@@ -15,12 +15,14 @@ class CountQueryStringBuilder extends JpqlQueryStringBuilder {
 
     @Override
     protected void appendSelectClause(final JpqlStringBuffer jpql, final String selectClause) {
-        jpql.appendToken("SELECT count(");
+        jpql.appendToken("SELECT");
+        jpql.appendToken("count(");
         if (selectClause.contains(KeyWord.Distinct.name())) {
             jpql.appendKeyWord(KeyWord.Distinct, false);
         }
         jpql.appendAlias();
-        jpql.appendToken(") FROM");
+        jpql.appendToken(")");
+        jpql.appendToken("FROM");
         jpql.appendEntity();
         jpql.appendAlias();
     }
